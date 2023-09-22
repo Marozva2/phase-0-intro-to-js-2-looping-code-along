c2becd684c17c4e435153755b4768815dd20a488
Marozva <isabokefrankline@gmail.com>
2023-09-22 19:21:28 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards() {
+    let names = ["Guadalupe", "Ollie", "Aki"]
+    let event = "birthday"
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+
+countDown = function() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
